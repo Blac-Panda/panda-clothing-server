8d9576d17e376332e8e43c76bd18e6b41f5f48ec
Blac-Panda <oyefesotunmise@gmail.com>
2019-08-21 12:48:36 +0100

    implemeted userReducer and UserAction to header and app components

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up'
 import {auth, createUserProfileDocument} from './firebase/firebase.utils'
 import {Switch, Route} from 'react-router-dom'
 
+import {connect} from 'react-redux'
+import {setCurrentUser} from './redux/user/user-actions'
+
 
 
 
@@ -22,23 +25,25 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount(){
+    const {setCurrentUser} = this.props;
+
     this.unsubscribeFromAuth =  
     auth.onAuthStateChanged(async userAuth => {
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
+          setCurrentUser({
               id: snapShot.id,
               ...snapShot.data()
             }
-          })
+            )
+          
         })
 
       }
 
-      this.setState({currentUser: userAuth});
+      setCurrentUser(userAuth);
     })
   }
 
@@ -49,7 +54,7 @@ class App extends React.Component {
   render() {
   return (
     <div>
-      <Header currentUser={this.state.currentUser} />
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
@@ -59,4 +64,8 @@ class App extends React.Component {
   );}
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
