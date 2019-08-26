782d7f0d952d2034a6ce9fa6427bc8bd1e14539c
Blac-Panda <oyefesotunmise@gmail.com>
2019-08-26 23:42:25 +0100

    updating aapp to redirect to home after signin

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ShopPage from './pages/shop/shop'
 import Header from './components/header/header'
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up'
 import {auth, createUserProfileDocument} from './firebase/firebase.utils'
-import {Switch, Route} from 'react-router-dom'
+import {Switch, Route, Redirect} from 'react-router-dom'
 
 import {connect} from 'react-redux'
 import {setCurrentUser} from './redux/user/user-actions'
@@ -58,14 +58,22 @@ class App extends React.Component {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInAndSignUp} />
+        <Route path="/signin" render={() => this.props.currentUser ? 
+          (<Redirect to='/'/>) 
+          : 
+          <SignInAndSignUp/>} 
+          />
       </Switch>
     </div>
   );}
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
