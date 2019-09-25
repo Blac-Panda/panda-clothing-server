cbb4078de157bfa6b5b6ca0770b159d738801e41
Blac-Panda <oyefesotunmise@gmail.com>
2019-09-25 19:38:55 +0100

    added increase/decrease button on checkout page

--- src/redux/cart/cart-utils.js
+++ src/redux/cart/cart-utils.js
@@ -1,3 +1,4 @@
+import cartActionTypes from "./cart.types";
 
 export const addItemToCart = (CartItems, cartItemToAdd) => {
     const existingCartItem = CartItems.find(
@@ -12,4 +13,20 @@ export const addItemToCart = (CartItems, cartItemToAdd) => {
     }
 
     return [...CartItems, {...cartItemToAdd, quantity: 1}]
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToRemove.id
+    );
+
+    if (existingCartItem.quantity === 1){ 
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+    }
+
+    return cartItems.map(
+        cartItem => cartItem.id === cartItemToRemove.id ?
+        {...cartItem, quantity: cartItem.quantity - 1}
+        : cartItem
+    );
 };
