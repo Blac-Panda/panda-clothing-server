cde4a00e789d5ab19de4bbea9cc2683bcbf586ad
Blac-Panda <oyefesotunmise@gmail.com>
2019-09-25 20:55:24 +0100

    issues with redux-persist unresolved

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
-
+import {PersistGate} from 'redux-persist/integration/react'
 import {Provider} from 'react-redux'
 import store from './redux/store'
+import persistor from './redux/store'
 
 ReactDOM.render(
 <Provider store={store}>
     <BrowserRouter>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
     </BrowserRouter>
 </Provider>
 , document.getElementById('root'));
