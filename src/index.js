6173dbe98d4c91145c5908d77c270c782af92728
Blac-Panda <oyefesotunmise@gmail.com>
2020-07-23 11:19:46 +0100

    fixed responsiveness issues

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import {store, persistor} from './redux/store'
 
 ReactDOM.render(
 <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="">
         <PersistGate persistor={persistor}>
             <App />
         </PersistGate>
